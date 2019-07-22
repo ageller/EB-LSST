24862021325b4f1e3e784296ef11847d51a1dded
Aaron Geller <a-geller@northwestern.edu>
2019-07-22 16:34:16 -0500

    bug fixes

--- code/StarClusterMPIdriver.py
+++ code/StarClusterMPIdriver.py
@@ -109,7 +109,7 @@ if __name__ == "__main__":
 	nClustersPerCore = int(np.floor(nClusters/size))
 	print(f"nClusters={nClusters}, nClustersPerCore={nClustersPerCore}")
 
-	nVals = 10 #I thouch this should be 11??
+	nVals = 11
 	sendbuf = np.empty((size, nVals*nClustersPerCore), dtype='float64')
 	recvbuf = np.empty(nVals*nClustersPerCore, dtype='float64')
 
