a6f73d1b16b8d21c846bfdb2daf9e94d1a6fd6d9
Aaron Geller <a-geller@northwestern.edu>
2019-10-21 15:35:40 -0500

    updated plot label

--- plotting/makeRatioPlots.py
+++ plotting/makeRatioPlots.py
@@ -94,4 +94,4 @@ if __name__ == "__main__":
 	plotHist(data['histAll'].values, data['histObs'].values, data['allhistRec'].values, data['binEdges'].values, r'r$_2$/r$_1$', 'EBLSST_rhist_final')
 
 	data = pd.read_csv('EBLSST_maghist.csv')
-	plotHist(data['histAll'].values, data['histObs'].values, data['allhistRec'].values, data['binEdges'].values, r'$_r$ [mag]', 'EBLSST_maghist_final')
+	plotHist(data['histAll'].values, data['histObs'].values, data['allhistRec'].values, data['binEdges'].values, r'$r_$ [mag]', 'EBLSST_maghist_final')
